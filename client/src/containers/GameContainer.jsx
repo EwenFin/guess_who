f9dd49b9f036fbc118ffaf0d37465c3bf39660b5
Ewen Carr <ewencarr@hotmail.com>
2017-05-05 10:30:13 +0100

    first commit

---
+++ client/src/containers/GameContainer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Board from '../components/Board.jsx'
+
+class GameContainer extends React.Component{
+
+}
+
+render(){
+  return(
+    <div>
+      <Board/>
+    </div>
+    )
+}
+
+export default BoardContainer
